53dcd590bfe9edc66e2a1775e27c1291449830aa
Rishi <risanand@cisco.com>
2017-12-26 18:39:14 +0530

    day 20 'Sorting'

---
+++ src/main/java/thirty/days/of/code/twenty/README.md
@@ -0,0 +1,11 @@
+[topic]
+Sorting
+
+[link]
+https://www.hackerrank.com/challenges/30-sorting/problem
+
+
+
+![Alt text](../../../../../../resources/thirty.days.of.code/question-20-1.png?raw=true "Title")
+![Alt text](../../../../../../resources/thirty.days.of.code/question-20-2.png?raw=true "Title")
+![Alt text](../../../../../../resources/thirty.days.of.code/question-20-3.png?raw=true "Title")
---
+++ src/main/java/thirty/days/of/code/twenty/Solution.java
@@ -0,0 +1,49 @@
+package thirty.days.of.code.twenty;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        // Write Your Code Here
+        swapArray(a);
+
+    }
+
+    private static void swapArray(int[] a){
+        int numberOfSwaps = 0;
+        for (int i = 0; i < a.length; i++) {
+            // Track number of elements swapped during a single array traversal
+
+
+            for (int j = 0; j < a.length - 1; j++) {
+                // Swap adjacent elements if they are in decreasing order
+                if (a[j] > a[j + 1]) {
+                    int tmp = a[j + 1];
+                    a[j + 1] = a[j];
+                    a[j] = tmp;
+                    numberOfSwaps++;
+                }
+            }
+
+            // If no elements were swapped during a traversal, array is sorted
+            if (numberOfSwaps == 0) {
+                break;
+            }
+        }
+
+        System.out.println("Array is sorted in " + numberOfSwaps + " swaps.");
+        System.out.println("First Element: " + a[0]);
+        System.out.println("Last Element: " + a[a.length - 1]);
+    }
+}
